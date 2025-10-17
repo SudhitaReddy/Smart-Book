f50e109814ff674e44e17e1b29ac631da5f776a9
Sudhita Reddy <sudhitareddy582@gmail.com>
2025-10-17 20:04:50 +0530

    Added proper .gitignore

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const Cart = require("../models/cart");
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const { protect } = require("../middleware/auth");
 
 const router = express.Router();
